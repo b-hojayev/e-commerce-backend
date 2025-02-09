aa6e3f11be6a1d12b46b3d31e8f40fdb5e473879
b-hojayev <bgnchojayew@gmail.com>
2025-02-09 16:48:15 +0500

    login route

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const cors = require("cors");
 
 //routers
 const authRouter = require("./routes/auth");
+
 //error-handlers
 const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require("./middleware/not-found");
 
 const app = express();
 
@@ -18,6 +20,7 @@ app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 
 app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware);
 
 const PORT = process.env.PORT || 5000;
 const start = () => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,13 @@
 const BadRequestError = require("../errors/bad-request");
+const db = require("../db/index");
+const jwt = require("jsonwebtoken");
+const { StatusCodes } = require("http-status-codes");
 
 function validatePhoneNumber(phone) {
   const regex = /^993(61|62|63|64|65|71)\d{6}$/;
   return regex.test(phone);
 }
+const fifteenDaysInMs = 15 * 24 * 60 * 60 * 1000;
 
 const register = async (req, res) => {
   const { first_name, last_name, phone } = req.body;
@@ -22,7 +26,33 @@ const register = async (req, res) => {
     );
   }
 
-  res.json(req.body);
+  const response = await db(
+    "INSERT INTO users(first_name, last_name, phone_number) VALUES($1, $2, $3) RETURNING *",
+    [first_name, last_name, phone]
+  );
+  const user = response.rows[0];
+
+  const accessToken = jwt.sign(
+    { id: user.id },
+    process.env.JWT_ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "1h",
+    }
+  );
+  const refreshToken = jwt.sign(
+    { id: user.id },
+    process.env.JWT_REFRESH_TOKEN_SECRET,
+    { expiresIn: "10d" }
+  );
+
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true,
+    sameSite: "None",
+    secure: true,
+    maxAge: fifteenDaysInMs,
+  });
+
+  return res.status(StatusCodes.OK).json({ accessToken });
 };
 
 const login = async (req, res) => {
